67ad079109676db5d02a3fbee3e02fa5256cec2f
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2021-07-18 08:08:06 +0700

    Verifymail is now working

--- pages/verifymail/email.php
+++ pages/verifymail/email.php
@@ -29,62 +29,127 @@
 
     <div class="search-container">
         <h1>Verify email address</h1>
-        <div class="wrapper">
-            <input class="search" placeholder="Enter email address here...." type="text" required>
-            <button class="button">Verify</button>
-        </div>
+        <form action="email.php" method="post">
+            <div class="wrapper">
+                <input class="search" placeholder="Enter email address here...." type="text" name="emailToCheck" required>
+                <button class="button" name="clickbuttonver">Verify</button>
+            </div>
+        </form>
     </div>
 
     <div class="validation-group">
         <div class="validation-container">
+            <h4>Email to check</h4>
+            <div class="box">
+                <p id="emailToCheck">-</p>
+            </div>
+        </div>
+
+        <div class="validation-container">
+            <h4>Deliverability</h4>
+            <div class="box">
+                <p id="deliverability">-</p>
+            </div>
+        </div>
+
+        <div class="validation-container">
+            <h4>Quality_score</h4>
+            <div class="box">
+                <p id="quality">-</p>
+            </div>
+        </div>
+
+        <div class="validation-container">
             <h4>Is this email valid?</h4>
             <div class="box">
-                <p class="box-default">Yes</p>
-                <p class="box-default">No</p>
+                <p class="box-default" id ="yesValid">Yes</p>
+                <p class="box-default" id ="noValid">No</p>
             </div>
         </div>
 
         <div class="validation-container">
-            <h4>Is this free email?</h4>
+            <h4>Is this email free?</h4>
             <div class="box">
-                <p class="box-default yes">Yes</p>
-                <p class="box-default">No</p>
+                <p class="box-default" id="yesFree">Yes</p>
+                <p class="box-default" id="noFree">No</p>
             </div>
         </div>
 
         <div class="validation-container">
-            <h4>Is this disposable email?</h4>
+            <h4>Is this email disposable?</h4>
             <div class="box">
-                <p class="box-default">Yes</p>
-                <p class="box-default no">No</p>
+                <p class="box-default" id="yesDisposable">Yes</p>
+                <p class="box-default" id="noDisposable">No</p>
             </div>
         </div>
 
         <div class="validation-container">
-            <h4>Is this role email??</h4>
+            <h4>Is this a role email??</h4>
             <div class="box">
-                <p class="box-default yes">Yes</p>
-                <p class="box-default">No</p>
+                <p class="box-default" id="yesRoleEmail">Yes</p>
+                <p class="box-default" id="noRoleEmail">No</p>
             </div>
         </div>
 
         <div class="validation-container">
-            <h4>Is this MX email?</h4>
+            <h4>Is this an MX email?</h4>
             <div class="box">
-                <p class="box-default">Yes</p>
-                <p class="box-default no">No</p>
+                <p class="box-default" id="yesMx">Yes</p>
+                <p class="box-default" id="noMx">No</p>
             </div>
         </div>
 
         <div class="validation-container">
             <h4>Is this email SMTP valid?</h4>
             <div class="box">
-                <p class="box-default">Yes</p>
-                <p class="box-default">No</p>
+                <p class="box-default" id="yesSmtp">Yes</p>
+                <p class="box-default" id="noSmtp">No</p>
             </div>
         </div>
 
     </div>
+
+    <?php 
+    require_once "./verifymail.php";
+    // button pressed
+    if (isset($_POST['clickbuttonver'])) {
+        $emailToCheck = $_POST['emailToCheck'];
+
+        list($email, $deliverability, $quality_score, $valid, $free, 
+        $disposable, $roleEmail, $mx, $smtp) = verify($emailToCheck);
+
+        echo "
+            <script>
+                document.getElementById('emailToCheck').innerHTML = '$email';
+                document.getElementById('deliverability').innerHTML = '$deliverability';
+                document.getElementById('quality').innerHTML = '$quality_score';
+            </script>
+        ";
+        replaceColor("Valid", $valid);
+        replaceColor("Free", $free);
+        replaceColor("Disposable", $disposable);
+        replaceColor("RoleEmail", $roleEmail);
+        replaceColor("Mx", $mx);
+        replaceColor("Smtp", $smtp);
+    }
+
+    function replaceColor($id_box, $isTrue){
+        if ($isTrue) { 
+            $color = "#00623B";
+            $id = 'yes'.$id_box;
+        } else {
+            $color = "#E20000";
+            $id = 'no'.$id_box;
+        }
+
+        echo "
+            <script>
+                document.getElementById('$id').style.backgroundColor = \"$color\";
+                document.getElementById('$id').style.color = 'white';
+            </script>
+        ";
+    }
+    ?>
 
     <?php include_once "../header_footer/footer.php" ?>
 
--- pages/verifymail/verifymail.php
+++ pages/verifymail/verifymail.php
@@ -1,41 +1,39 @@
 <?php 
     require_once __DIR__ . '/../../handler/php/loadenv.php';
 
-    $emailToCheck = 'testmail@gmail.com';
+    function verify($emailToCheck){
+        // Initialize cURL.
+        $ch = curl_init();
 
-    // Initialize cURL.
-    $ch = curl_init();
+        // Set the URL that you want to GET by using the CURLOPT_URL option.
+        curl_setopt($ch, CURLOPT_URL, "https://emailvalidation.abstractapi.com/v1/?api_key={$_ENV["VERIFY_MAIL_KEYS"]}&email={$emailToCheck}");
 
-    // Set the URL that you want to GET by using the CURLOPT_URL option.
-    curl_setopt($ch, CURLOPT_URL, "https://emailvalidation.abstractapi.com/v1/?api_key={$_ENV["VERIFY_MAIL_KEYS"]}&email={$emailToCheck}");
+        // Set CURLOPT_RETURNTRANSFER so that the content is returned as a variable.
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-    // Set CURLOPT_RETURNTRANSFER so that the content is returned as a variable.
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // Set CURLOPT_FOLLOWLOCATION to true to follow redirects.
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
-    // Set CURLOPT_FOLLOWLOCATION to true to follow redirects.
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        // Execute the request.
+        $data = curl_exec($ch);
 
-    // Execute the request.
-    $data = curl_exec($ch);
+        // Parse data to json object
+        $dataJson = json_decode($data);
 
-    // Parse data to json object
-    $dataJson = json_decode($data);
+        // Close the cURL handle.
+        curl_close($ch);
 
-    // Close the cURL handle.
-    curl_close($ch);
+        // Print the data out onto the page.
+        $email = $dataJson->email;
+        $deliverability = $dataJson->deliverability;
+        $quality_score = $dataJson->quality_score;
+        $valid = $dataJson->is_valid_format->value;
+        $free = $dataJson->is_free_email->value;
+        $disposeable = $dataJson->is_disposable_email->value;
+        $roleEmail = $dataJson->is_role_email->value;
+        $mx = $dataJson->is_mx_found->value;
+        $smtp = $dataJson->is_smtp_valid->value;
 
-    // Print the data out onto the page.
-    $email = $dataJson->email;
-
-    echo ("Email\t\t: " . $dataJson->email . "\n");
-    echo ("Deliverability\t: " . $dataJson->deliverability . "\n");
-    echo ("Quality_score\t: " . $dataJson->quality_score . "\n");
-    echo ("Is It Valid?\t\t\t\t" . ($dataJson->is_valid_format->value ? "Yes" : "No") . "\n");
-    echo ("Is It Free?\t\t\t\t" . ($dataJson->is_free_email->value ? "Yes" : "No") . "\n");
-    echo ("Is It Disposable?\t\t\t" . ($dataJson->is_disposable_email->value ? "Yes" : "No") . "\n");
-    echo ("Is It a Role Email? (Company Address)\t" . ($dataJson->is_role_email->value ? "Yes" : "No")  . "\n");
-    echo ("Is mx Found?\t\t\t\t" . ($dataJson->is_mx_found->value ? "Yes" : "No")  . "\n");
-    echo ("Is it SMTP Valid?\t\t\t" . ($dataJson->is_smtp_valid->value ? "Yes" : "No")  . "\n");
-
-    // masih backend
+        return array($email, $deliverability, $quality_score, $valid, $free, $disposeable, $roleEmail, $mx, $smtp);
+    };
 ?>
