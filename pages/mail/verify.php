7dfb8bec09eca1b82cd54d6551db1221e08e7d7a
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2021-07-19 23:59:22 +0700

    Update verify.php
    - icon fix

--- pages/mail/verify.php
+++ pages/mail/verify.php
@@ -19,7 +19,6 @@
     <link rel="stylesheet" href="../../handler/css/style.css?v=<? echo time(); ?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="icon" href="../" ;>
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
         rel="stylesheet">
 </head>
