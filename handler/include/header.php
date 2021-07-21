f77517a47ba1fd8521c44dc4adb3f2e0c5276bc4
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2021-07-21 14:31:00 +0700

    Improve header
    - more centered
    - The mobile view looks somewhat better

--- handler/include/header.php
+++ handler/include/header.php
@@ -68,13 +68,13 @@
                 <li class=\"list-inline-item-mobile\">
                     <a href=\"{$dirHome}\">Home</a>
                 </li>
-                <li class=\"list-inline-item-mobile\">
+                <li class=\"list-inline-item-mobile push-Mleft-little\">
                     <a href=\"{$dirNews}\">News</a>
                 </li>
-                <li class=\"list-inline-item-mobile\">
+                <li class=\"list-inline-item-mobile push-Mleft-little\">
                     <a href=\"{$dirAbout}\">About</a>
                 </li>
-                <li class=\"list-inline-item-mobile\">
+                <li class=\"list-inline-item-mobile push-Mleft-little\">
                     <a href=\"{$dirTeam}\">Team</a>
                 </li>
             </ul>
