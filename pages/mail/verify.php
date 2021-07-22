fb3902147e178d365cfc1ee989ae0b84cc7f8a22
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2021-07-22 12:21:39 +0700

    Added recaptcha v2

--- pages/mail/verify.php
+++ pages/mail/verify.php
@@ -21,6 +21,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
         rel="stylesheet">
+    <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 
 <body class="background bulat1 bulat2">
@@ -39,6 +40,15 @@
                         pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" title="Please enter a valid email address"
                         minlength="3" required>
                     <button class="button" name="clickbuttonver">Verify</button>
+                </div>
+                <div class="center-recaptcha">
+                <?php 
+                require_once '../../handler/require/loadenv.php';
+                echo"
+                    <div class=\"g-recaptcha\" data-sitekey=\"{$_ENV["recaptchaSiteKey"]}\"></div>
+                " 
+                ?>
+                    
                 </div>
             </form>
         </div>
@@ -117,28 +127,67 @@
 
     <?php 
     require_once "../../handler/require/verifyMail.php";
-    // button pressed
+    // Check When Button Is Pressed
+    // Plus server side validation
     if (isset($_POST['clickbuttonver']) && !empty($_POST['emailToCheck']) &&
     preg_match('/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i', $_POST['emailToCheck'])) {
-        // get email address
-        $emailToCheck = $_POST['emailToCheck'];
+        $result = CheckCaptcha($_POST['g-recaptcha-response']);
 
-        list($email, $deliverability, $quality_score, $valid, $free, 
-        $disposable, $roleEmail, $mx, $smtp) = verify($emailToCheck);
+        /* DEBUG 
+        echo json_encode($result, JSON_PRETTY_PRINT); */
 
-        echo "
-            <script>
-                document.getElementById('emailToCheck').innerHTML = '$email';
-                document.getElementById('deliverability').innerHTML = '$deliverability';
-                document.getElementById('quality').innerHTML = '$quality_score';
-            </script>
-        ";
-        replaceColor("Valid", $valid);
-        replaceColor("Free", $free);
-        replaceColor("Disposable", $disposable);
-        replaceColor("RoleEmail", $roleEmail);
-        replaceColor("Mx", $mx);
-        replaceColor("Smtp", $smtp);
+        // If success
+        if ($result->success) {
+            // get email address
+            $emailToCheck = $_POST['emailToCheck'];
+
+            // Get the data
+            list($email, $deliverability, $quality_score, $valid, $free, 
+            $disposable, $roleEmail, $mx, $smtp) = verify($emailToCheck);
+
+            echo "
+                <script>
+                    document.getElementById('emailToCheck').innerHTML = '$email';
+                    document.getElementById('deliverability').innerHTML = '$deliverability';
+                    document.getElementById('quality').innerHTML = '$quality_score';
+                </script>
+            ";
+            replaceColor("Valid", $valid);
+            replaceColor("Free", $free);
+            replaceColor("Disposable", $disposable);
+            replaceColor("RoleEmail", $roleEmail);
+            replaceColor("Mx", $mx);
+            replaceColor("Smtp", $smtp);
+        } else {
+            // If the CAPTCHA box wasn't checked
+           echo '<script>alert("Error! - Please Verify The Recaptcha First!");</script>';
+        }
+    }
+
+    function CheckCaptcha($userResponse) {
+        require_once '../../handler/require/loadenv.php';
+
+        $fields_string = '';
+        $fields = array(
+            'secret' => $_ENV["recaptchaSecretKey"],
+            'response' => $userResponse
+        );
+        foreach($fields as $key=>$value)
+        $fields_string .= $key . '=' . $value . '&';
+        $fields_string = rtrim($fields_string, '&');
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+        curl_setopt($ch, CURLOPT_POST, count($fields));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
+
+        $res = curl_exec($ch);
+        curl_close($ch);
+
+        $jsonRes = json_decode($res);
+
+        return $jsonRes;
     }
 
     function replaceColor($id_box, $isTrue){
