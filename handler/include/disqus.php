e063ddcc399f527bf1e90e0d4f7128bb1a8937a9
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2021-08-03 18:25:12 +0700

    Add button to show comments

--- handler/include/disqus.php
+++ handler/include/disqus.php
@@ -1,20 +1,28 @@
-<?php 
-    function disqus_thread($pageURL, $pageIdentifier) {
-echo "
-        <div id=\"disqus_thread\"></div>
-            <script>
-                var disqus_config = function () {
-                this.page.url = '$pageURL';
-                this.page.identifier = '$pageIdentifier';
-                };
-                
-                (function() { // DON'T EDIT BELOW THIS LINE
-                var d = document, s = d.createElement('script');
+<?php
+
+function disqus_thread($pageURL, $pageIdentifier) {
+    echo " 
+        <div id = \"disqus_thread\">
+            <h4 class=\"comments-holder\"><a href=\"#\" onclick=\"loadDisqus();return false;\">Show/Post Comments</a></h4>
+        </div>
+ 
+        <script>
+        var disqus_config = function () {
+            this.page.url = '$pageURL';
+            this.page.identifier = '$pageIdentifier';
+        };
+        var is_disqus_loaded = false;
+        function loadDisqus() {
+            if (!is_disqus_loaded) {
+                is_disqus_loaded = true;
+                var d = document,
+                    s = d.createElement('script');
                 s.src = 'https://letsapi.disqus.com/embed.js';
                 s.setAttribute('data-timestamp', +new Date());
                 (d.head || d.body).appendChild(s);
-                })();
-            </script>
-            <noscript>Please enable JavaScript to view the <a href=\"https://disqus.com/?ref_noscript\">comments powered by Disqus.</a></noscript>
-";}
+            }
+        }; 
+        </script> 
+        <noscript> Please enable JavaScript to view the <a href=\"https://disqus.com/?ref_noscript\">comments powered by Disqus.</a></noscript>
+    ";} 
 ?>
