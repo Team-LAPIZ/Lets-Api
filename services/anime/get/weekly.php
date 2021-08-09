7073144a244a18743cd7d79546567a03128d88f2
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2021-08-09 13:00:04 +0700

    weekly schedule

--- services/anime/get/weekly.php
+++ services/anime/get/weekly.php
@@ -23,19 +23,246 @@ function getDay($day){
 function monday() {
     $data = getDay("monday");
 
-    for ($i=0; $i < count($data); $i++) { 
+    for ($i=0; $i < count($data->monday); $i++) { 
+        $id = $data->monday[$i]->mal_id;
         $title = $data->monday[$i]->title;
         $score = $data->monday[$i]->score ? $data->monday[$i]->score : "N/A";
         $members = $data->monday[$i]->members ? $data->monday[$i]->members : "N/A";
         $synopsis = $data->monday[$i]->synopsis ? $data->monday[$i]->synopsis : "N/A";
+        $source = $data->monday[$i]->source ? $data->monday[$i]->source : "N/A";
         $type = $data->monday[$i]->type ? $data->monday[$i]->type : "N/A";
         $airing_start = $data->monday[$i]->airing_start ? $data->monday[$i]->airing_start : "N/A";
         $episodes = $data->monday[$i]->episodes ? $data->monday[$i]->episodes : "N/A";
-
-        
+        if($airing_start !== "N/A") {
+            $formatted = date_create($airing_start);
+            $airing_start = $formatted->format('M d, Y');
+        }
+        echo"
+        <a href=\"./details?id={$id}\" data-bs-toggle=\"tooltip\" data-bs-placement=\"auto\" data-bs-html=\"true\"
+        title=\"Score: {$score} <br>Members: #{$members}<br>Episodes: {$episodes}<br>Start Airing on: {$airing_start}<br>Source: {$source}<br>Type:{$type} \">
+            <figure class=\"item-img\">
+                <div class=\"img__wrap\">
+                <img class=\"img__img\" src=\"{$data->monday[$i]->image_url}\" onerror=\"this.onerror=null; this.src='/handler/img/noposter.png'\" alt=\"{$title} Poster\"/>
+                <div class=\"top-left\">Score :{$score}</div>
+                    <div class=\"img__description_layer\">
+                        <p class=\"img__description\">{$synopsis}</p>
+                    </div>
+                </div>
+                <figcaption class=\"item-caption\">$title</figcaption>
+            </figure>
+        </a>
+        ";
     }
 }
 
+function tuesday(){
+    $data = getDay("tuesday");
 
+    for ($i=0; $i < count($data->tuesday); $i++) { 
+        $id = $data->tuesday[$i]->mal_id;
+        $title = $data->tuesday[$i]->title;
+        $score = $data->tuesday[$i]->score ? $data->tuesday[$i]->score : "N/A";
+        $members = $data->tuesday[$i]->members ? $data->tuesday[$i]->members : "N/A";
+        $synopsis = $data->tuesday[$i]->synopsis ? $data->tuesday[$i]->synopsis : "N/A";
+        $source = $data->tuesday[$i]->source ? $data->tuesday[$i]->source : "N/A";
+        $type = $data->tuesday[$i]->type ? $data->tuesday[$i]->type : "N/A";
+        $airing_start = $data->tuesday[$i]->airing_start ? $data->tuesday[$i]->airing_start : "N/A";
+        $episodes = $data->tuesday[$i]->episodes ? $data->tuesday[$i]->episodes : "N/A";
+        if($airing_start !== "N/A") {
+            $formatted = date_create($airing_start);
+            $airing_start = $formatted->format('M d, Y');
+        }
+        echo"
+        <a href=\"./details?id={$id}\" data-bs-toggle=\"tooltip\" data-bs-placement=\"auto\" data-bs-html=\"true\"
+        title=\"Score: {$score} <br>Members: #{$members}<br>Episodes: {$episodes}<br>Start Airing on: {$airing_start}<br>Source: {$source}<br>Type:{$type} \">
+            <figure class=\"item-img\">
+                <div class=\"img__wrap\">
+                <img class=\"img__img\" src=\"{$data->tuesday[$i]->image_url}\" onerror=\"this.onerror=null; this.src='/handler/img/noposter.png'\" alt=\"{$title} Poster\"/>
+                <div class=\"top-left\">Score :{$score}</div>
+                    <div class=\"img__description_layer\">
+                        <p class=\"img__description\">{$synopsis}</p>
+                    </div>
+                </div>
+                <figcaption class=\"item-caption\">$title</figcaption>
+            </figure>
+        </a>
+        ";
+    }
+}
+
+function wednesday(){
+    $data = getDay("wednesday");
+
+    for ($i=0; $i < count($data->wednesday); $i++) { 
+        $id = $data->wednesday[$i]->mal_id;
+        $title = $data->wednesday[$i]->title;
+        $score = $data->wednesday[$i]->score ? $data->wednesday[$i]->score : "N/A";
+        $members = $data->wednesday[$i]->members ? $data->wednesday[$i]->members : "N/A";
+        $synopsis = $data->wednesday[$i]->synopsis ? $data->wednesday[$i]->synopsis : "N/A";
+        $source = $data->wednesday[$i]->source ? $data->wednesday[$i]->source : "N/A";
+        $type = $data->wednesday[$i]->type ? $data->wednesday[$i]->type : "N/A";
+        $airing_start = $data->wednesday[$i]->airing_start ? $data->wednesday[$i]->airing_start : "N/A";
+        $episodes = $data->wednesday[$i]->episodes ? $data->wednesday[$i]->episodes : "N/A";
+        if($airing_start !== "N/A") {
+            $formatted = date_create($airing_start);
+            $airing_start = $formatted->format('M d, Y');
+        }
+        echo"
+        <a href=\"./details?id={$id}\" data-bs-toggle=\"tooltip\" data-bs-placement=\"auto\" data-bs-html=\"true\"
+        title=\"Score: {$score} <br>Members: #{$members}<br>Episodes: {$episodes}<br>Start Airing on: {$airing_start}<br>Source: {$source}<br>Type:{$type} \">
+            <figure class=\"item-img\">
+                <div class=\"img__wrap\">
+                <img class=\"img__img\" src=\"{$data->wednesday[$i]->image_url}\" onerror=\"this.onerror=null; this.src='/handler/img/noposter.png'\" alt=\"{$title} Poster\"/>
+                <div class=\"top-left\">Score :{$score}</div>
+                    <div class=\"img__description_layer\">
+                        <p class=\"img__description\">{$synopsis}</p>
+                    </div>
+                </div>
+                <figcaption class=\"item-caption\">$title</figcaption>
+            </figure>
+        </a>
+        ";
+    }
+}
+
+function thursday(){
+    $data = getDay("thursday");
+
+    for ($i=0; $i < count($data->thursday); $i++) { 
+        $id = $data->thursday[$i]->mal_id;
+        $title = $data->thursday[$i]->title;
+        $score = $data->thursday[$i]->score ? $data->thursday[$i]->score : "N/A";
+        $members = $data->thursday[$i]->members ? $data->thursday[$i]->members : "N/A";
+        $synopsis = $data->thursday[$i]->synopsis ? $data->thursday[$i]->synopsis : "N/A";
+        $source = $data->thursday[$i]->source ? $data->thursday[$i]->source : "N/A";
+        $type = $data->thursday[$i]->type ? $data->thursday[$i]->type : "N/A";
+        $airing_start = $data->thursday[$i]->airing_start ? $data->thursday[$i]->airing_start : "N/A";
+        $episodes = $data->thursday[$i]->episodes ? $data->thursday[$i]->episodes : "N/A";
+        if($airing_start !== "N/A") {
+            $formatted = date_create($airing_start);
+            $airing_start = $formatted->format('M d, Y');
+        }
+        echo"
+        <a href=\"./details?id={$id}\" data-bs-toggle=\"tooltip\" data-bs-placement=\"auto\" data-bs-html=\"true\"
+        title=\"Score: {$score} <br>Members: #{$members}<br>Episodes: {$episodes}<br>Start Airing on: {$airing_start}<br>Source: {$source}<br>Type:{$type} \">
+            <figure class=\"item-img\">
+                <div class=\"img__wrap\">
+                <img class=\"img__img\" src=\"{$data->thursday[$i]->image_url}\" onerror=\"this.onerror=null; this.src='/handler/img/noposter.png'\" alt=\"{$title} Poster\"/>
+                <div class=\"top-left\">Score :{$score}</div>
+                    <div class=\"img__description_layer\">
+                        <p class=\"img__description\">{$synopsis}</p>
+                    </div>
+                </div>
+                <figcaption class=\"item-caption\">$title</figcaption>
+            </figure>
+        </a>
+        ";
+    }
+}
+
+function friday(){
+    $data = getDay("friday");
+
+    for ($i=0; $i < count($data->friday); $i++) { 
+        $id = $data->friday[$i]->mal_id;
+        $title = $data->friday[$i]->title;
+        $score = $data->friday[$i]->score ? $data->friday[$i]->score : "N/A";
+        $members = $data->friday[$i]->members ? $data->friday[$i]->members : "N/A";
+        $synopsis = $data->friday[$i]->synopsis ? $data->friday[$i]->synopsis : "N/A";
+        $source = $data->friday[$i]->source ? $data->friday[$i]->source : "N/A";
+        $type = $data->friday[$i]->type ? $data->friday[$i]->type : "N/A";
+        $airing_start = $data->friday[$i]->airing_start ? $data->friday[$i]->airing_start : "N/A";
+        $episodes = $data->friday[$i]->episodes ? $data->friday[$i]->episodes : "N/A";
+        if($airing_start !== "N/A") {
+            $formatted = date_create($airing_start);
+            $airing_start = $formatted->format('M d, Y');
+        }
+        echo"
+        <a href=\"./details?id={$id}\" data-bs-toggle=\"tooltip\" data-bs-placement=\"auto\" data-bs-html=\"true\"
+        title=\"Score: {$score} <br>Members: #{$members}<br>Episodes: {$episodes}<br>Start Airing on: {$airing_start}<br>Source: {$source}<br>Type:{$type} \">
+            <figure class=\"item-img\">
+                <div class=\"img__wrap\">
+                <img class=\"img__img\" src=\"{$data->friday[$i]->image_url}\" onerror=\"this.onerror=null; this.src='/handler/img/noposter.png'\" alt=\"{$title} Poster\"/>
+                <div class=\"top-left\">Score :{$score}</div>
+                    <div class=\"img__description_layer\">
+                        <p class=\"img__description\">{$synopsis}</p>
+                    </div>
+                </div>
+                <figcaption class=\"item-caption\">$title</figcaption>
+            </figure>
+        </a>
+        ";
+    }
+}
+
+function saturday(){
+    $data = getDay("saturday");
+
+    for ($i=0; $i < count($data->saturday); $i++) { 
+        $id = $data->saturday[$i]->mal_id;
+        $title = $data->saturday[$i]->title;
+        $score = $data->saturday[$i]->score ? $data->saturday[$i]->score : "N/A";
+        $members = $data->saturday[$i]->members ? $data->saturday[$i]->members : "N/A";
+        $synopsis = $data->saturday[$i]->synopsis ? $data->saturday[$i]->synopsis : "N/A";
+        $source = $data->saturday[$i]->source ? $data->saturday[$i]->source : "N/A";
+        $type = $data->saturday[$i]->type ? $data->saturday[$i]->type : "N/A";
+        $airing_start = $data->saturday[$i]->airing_start ? $data->saturday[$i]->airing_start : "N/A";
+        $episodes = $data->saturday[$i]->episodes ? $data->saturday[$i]->episodes : "N/A";
+        if($airing_start !== "N/A") {
+            $formatted = date_create($airing_start);
+            $airing_start = $formatted->format('M d, Y');
+        }
+        echo"
+        <a href=\"./details?id={$id}\" data-bs-toggle=\"tooltip\" data-bs-placement=\"auto\" data-bs-html=\"true\"
+        title=\"Score: {$score} <br>Members: #{$members}<br>Episodes: {$episodes}<br>Start Airing on: {$airing_start}<br>Source: {$source}<br>Type:{$type} \">
+            <figure class=\"item-img\">
+                <div class=\"img__wrap\">
+                <img class=\"img__img\" src=\"{$data->saturday[$i]->image_url}\" onerror=\"this.onerror=null; this.src='/handler/img/noposter.png'\" alt=\"{$title} Poster\"/>
+                <div class=\"top-left\">Score :{$score}</div>
+                    <div class=\"img__description_layer\">
+                        <p class=\"img__description\">{$synopsis}</p>
+                    </div>
+                </div>
+                <figcaption class=\"item-caption\">$title</figcaption>
+            </figure>
+        </a>
+        ";
+    }
+}
+
+function sunday(){
+    $data = getDay("sunday");
+
+    for ($i=0; $i < count($data->sunday); $i++) { 
+        $id = $data->sunday[$i]->mal_id;
+        $title = $data->sunday[$i]->title;
+        $score = $data->sunday[$i]->score ? $data->sunday[$i]->score : "N/A";
+        $members = $data->sunday[$i]->members ? $data->sunday[$i]->members : "N/A";
+        $synopsis = $data->sunday[$i]->synopsis ? $data->sunday[$i]->synopsis : "N/A";
+        $source = $data->sunday[$i]->source ? $data->sunday[$i]->source : "N/A";
+        $type = $data->sunday[$i]->type ? $data->sunday[$i]->type : "N/A";
+        $airing_start = $data->sunday[$i]->airing_start ? $data->sunday[$i]->airing_start : "N/A";
+        $episodes = $data->sunday[$i]->episodes ? $data->sunday[$i]->episodes : "N/A";
+        if($airing_start !== "N/A") {
+            $formatted = date_create($airing_start);
+            $airing_start = $formatted->format('M d, Y');
+        }
+        echo"
+        <a href=\"./details?id={$id}\" data-bs-toggle=\"tooltip\" data-bs-placement=\"auto\" data-bs-html=\"true\"
+        title=\"Score: {$score} <br>Members: #{$members}<br>Episodes: {$episodes}<br>Start Airing on: {$airing_start}<br>Source: {$source}<br>Type:{$type} \">
+            <figure class=\"item-img\">
+                <div class=\"img__wrap\">
+                <img class=\"img__img\" src=\"{$data->sunday[$i]->image_url}\" onerror=\"this.onerror=null; this.src='/handler/img/noposter.png'\" alt=\"{$title} Poster\"/>
+                <div class=\"top-left\">Score :{$score}</div>
+                    <div class=\"img__description_layer\">
+                        <p class=\"img__description\">{$synopsis}</p>
+                    </div>
+                </div>
+                <figcaption class=\"item-caption\">$title</figcaption>
+            </figure>
+        </a>
+        ";
+    }
+}
 
 ?>
--- services/anime/schedule.php
+++ services/anime/schedule.php
@@ -4,7 +4,7 @@
 <head>
     <?php 
         include_once $_SERVER['DOCUMENT_ROOT'] . "/handler/include/meta.php";
-        importMeta('services/anime/' . basename($_SERVER['PHP_SELF']), "Anime Schedule - Let's API", "Anime, Scraper, API, LetsAPI, Tools", 
+        importMeta('services/anime/' . basename($_SERVER['PHP_SELF']), "Anime Weekly Schedule - Let's API", "Anime, Scraper, API, LetsAPI, Tools", 
         "Look up anime weekly schedule by using Jikan Rest API by Let's API, A website that utilize many API(s) and tools.");
     ?>
 </head>
@@ -16,69 +16,106 @@
 
     <div class="content">
         <div class="service-container">
-            <h1>Search Anime</h1>
-            <form method="get" action="search.php">
-                <div class="wrapper">
-                    <input class="service-container-search" placeholder="Search Anime...." type="text" name="anime"
-                        minlength="3" required>
-                    <button class="button">Search</button>
-                </div>
-            </form>
+            <h1>Weekly Schedule</h1>
         </div>
 
-        <div class="anime-container weekly">
-            <div class="days">
-                <h4 class="gallery-title">Monday</h4>
-                <div class="js-flickity" data-flickity-options='{ "cellAlign": "center", "autoPlay": 4000, "pauseAutoPlayOnHover": true, "groupCells": true, "wrapAround": true, "pageDots": false, 
-                    "prevNextButtons": true, "freeScroll": true, "selectedAttraction": 0.01, "friction": 0.20}'>
+        <div class="anime-container weekly" id="container">
+            <div class="days" id="monday">
+                <h3 class="schedule-title">Monday</h3>
+                <div class="schedule">
                     <?php require "./get/weekly.php";
-
+                        monday();
                     ?>
                 </div>
             </div>
-            <div class="days">
-                <h4 class="gallery-title">Tuesday</h4>
-                <div class="js-flickity" data-flickity-options='{ "cellAlign": "center", "autoPlay": 4000, "pauseAutoPlayOnHover": true, "groupCells": true, "wrapAround": true, "pageDots": false, 
-                    "prevNextButtons": true, "freeScroll": true, "selectedAttraction": 0.01, "friction": 0.20}'>
+            <div class="days" id="tuesday">
+                <h3 class="schedule-title">Tuesday</h3>
+                <div class="schedule">
                     <?php
-
+                        tuesday();
                     ?>
                 </div>
             </div>
-            <div class="days">
-                <h4 class="gallery-title">Wednesday</h4>
-                <div class="js-flickity" data-flickity-options='{ "cellAlign": "center", "autoPlay": 4000, "pauseAutoPlayOnHover": true, "groupCells": true, "wrapAround": true, "pageDots": false, 
-                    "prevNextButtons": true, "freeScroll": true, "selectedAttraction": 0.01, "friction": 0.20}'>
+            <div class="days" id="wednesday">
+                <h3 class="schedule-title">Wednesday</h3>
+                <div class="schedule">
                     <?php
-
+                        wednesday();
                     ?>
                 </div>
             </div>
-            <div class="days">
-                <h4 class="gallery-title">Thursday</h4>
-                <div class="js-flickity" data-flickity-options='{ "cellAlign": "center", "autoPlay": 4000, "pauseAutoPlayOnHover": true, "groupCells": true, "wrapAround": true, "pageDots": false, 
-                    "prevNextButtons": true, "freeScroll": true, "selectedAttraction": 0.01, "friction": 0.20}'>
+            <div class="days" id="thursday">
+                <h3 class="schedule-title">Thursday</h3>
+                <div class="schedule">
                     <?php
-
+                        thursday();
                     ?>
                 </div>
             </div>
-            <div class="days">
-                <h4 class="gallery-title">Saturday</h4>
-                <div class="js-flickity" data-flickity-options='{ "cellAlign": "center", "autoPlay": 4000, "pauseAutoPlayOnHover": true, "groupCells": true, "wrapAround": true, "pageDots": false, 
-                    "prevNextButtons": true, "freeScroll": true, "selectedAttraction": 0.01, "friction": 0.20}'>
+            <div class="days" id="friday">
+                <h3 class="schedule-title">Friday</h3>
+                <div class="schedule">
                     <?php
-
+                        friday();
+                    ?>
+                </div>
+            </div>
+            <div class="days" id="saturday">
+                <h3 class="schedule-title">Saturday</h3>
+                <div class="schedule">
+                    <?php
+                        saturday();
+                    ?>
+                </div>
+            </div>
+            <div class="days" id="sunday">
+                <h3 class="schedule-title">Sunday</h3>
+                <div class="schedule">
+                    <?php
+                        sunday();
                     ?>
                 </div>
             </div>
         </div>
     </div>
 
+    <script>
+        var mon = document.getElementById('monday');
+        var tues = document.getElementById('tuesday');
+        var wed = document.getElementById('wednesday');
+        var thurs = document.getElementById('thursday');
+        var fri = document.getElementById('friday');
+        var sat = document.getElementById('saturday');
+        var sun = document.getElementById('sunday');
+
+        function setBorder(el) {
+            el.style.borderTop = "1px solid #189AB4";
+        }
+
+        function removeBorder(el) {
+            el.style.borderTop = "none";
+        }
+
+        window.onresize = function() { 
+            if(getScreenWidth() < 826) {
+                setBorder(mon); setBorder(tues); setBorder(wed);
+                setBorder(thurs); setBorder(fri); setBorder(sat);
+                setBorder(sun);
+            } else {
+                removeBorder(mon); removeBorder(tues); removeBorder(wed); 
+                removeBorder(thurs); removeBorder(fri); removeBorder(sat);
+                removeBorder(sun);
+            }
+        }
+        
+        // Run when start
+        window.onresize();
+    </script>
+
     <?php 
         include_once $_SERVER['DOCUMENT_ROOT'] . "/handler/include/footer.php";
         footerImport('services/anime/' . basename($_SERVER['PHP_SELF']),  "This service is made possible by using <a class=\"footerlink\" href=\"https://jikan.moe/\" target=\"_blank\" rel=\"noreferrer noopener\">Jikan API</a>, 
-        a <a class=\"footerlink\" href=\"https://myanimelist.net/\" target=\"_blank\" rel=\"noreferrer noopener\">MAL</a> scraper Rest API. Images gallery are made using <a class=\"footerlink\" href=\"https://flickity.metafizzy.co/\" target=\"_blank\" rel=\"noreferrer noopener\">Flickity</a>");
+        a <a class=\"footerlink\" href=\"https://myanimelist.net/\" target=\"_blank\" rel=\"noreferrer noopener\">MAL</a> scraper Rest API.");
     ?>
 </body>
 
