2c319339d0c954e6b939f1f1649cb6cccc550771
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2021-08-14 13:51:45 +0700

    Update details.php

--- services/anime/details.php
+++ services/anime/details.php
@@ -119,7 +119,7 @@
                 </div>
                 <div class="details-content">
                     <h4 class="gallery-title"><span class="gallery-border-bot">Synopsis</span></h4>
-                    <div class="synopsis" style="padding-top: 1px;">
+                    <div class="synopsis" style="padding-top: 0.5px;">
                         <p class="p-justify"><?php echo $synopsis ?></p>
                     </div>
                 </div>
